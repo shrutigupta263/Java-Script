91eb3c95d9249955552ee012dc7fe776ed7eb06b
shrutigupta263 <shrutigupptaab263@gmail.com>
2023-10-09 00:42:31 +0530

    conversion of datatypes

---
+++ conversiondataype.js
@@ -0,0 +1,51 @@
+//string because of double inveeted comma //
+
+/*let score = "22"
+console.log(typeof score);
+console.log(typeof (score));
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);*/
+
+//because of no inverted comma//
+
+/*let score = 22
+console.log(typeof score);
+console.log(typeof (score));
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);*/
+
+//boolean expresion//
+
+/*let score = true
+console.log(typeof score);
+console.log(typeof (score));
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);*/
+
+//name//
+
+/*let score = "shruti"
+console.log(typeof score);
+console.log(typeof (score));
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);*/
+
+//number and alphabet//
+
+/*let score = "33aen"
+console.log(typeof score);
+console.log(typeof (score));
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);*/
+
+//coversion of number into alphaa//
+
+let isLoggedIn = 1
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+//here 1 is true and 0 is zero but empty is false and name is true//
