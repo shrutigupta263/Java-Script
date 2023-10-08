80ca6609f43e4d622aff270f4fe107c6e1ef27a3
shrutigupta263 <shrutigupptaab263@gmail.com>
2023-10-09 03:01:37 +0530

    operation sign of maths

---
+++ operation.js
@@ -0,0 +1,26 @@
+let str1 = "hello"
+let str2 = "shruti"
+let str3 = str1 + str2
+console.log(str3);
+console.log("1" + 2);// here result will be 12 because of string in double inverted comma //
+console.log(1 + "2");// here also samee onlyy//
+console.log(+true);//because of true value 1//
+console.log(+"");//undefine therefore 0//
+//predefine //
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter);
+
+//comparison of dtattypes//
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+
+// === 
+
+console.log("2" === 2);
