e0f69fd6b87bdc2fb71a64184d806889be30c218
shrutigupta263 <shrutigupptaab263@gmail.com>
2023-10-08 19:19:21 +0530

    variable define

---
+++ variable.js
@@ -0,0 +1,11 @@
+ const accountId= 123
+ let accountEmail = "shruti@gmail.com"
+ var accountPassword = "1234"
+ accountCity = "Jaipur"
+ let accountState;
+ // const means no chages for given value//
+ // let and var are variable used but let is constantly used most//
+accountEmail = "shruti123@gmail.com"
+accountPassword = "1234455"
+accountCity =  "pune" 
+ console.table([accountId, accountEmail, accountPassword, accountCity, accountState ])
