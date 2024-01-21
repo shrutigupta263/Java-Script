9bfc04010479b03d63a6d4e52f567b301218e9f8
shrutigupta263 <shrutigupptaab263@gmail.com>
2024-01-22 00:46:41 +0530

    number

---
+++ number.js
@@ -0,0 +1,11 @@
+const score = 400
+console.log(score);
+const balance = new Number(100) //shows number type like string 
+console.log(balance);
+console.log(balance.toString().length); //length of no. 
+console.log(balance.toFixed(1)); // fixed value not changee anyy
+
+const othernumber = 123.56
+console.log(othernumber.toPrecision(4)); //maths propertyy 4 no. show place of no.
+ 
+
