419c66dd00f338b095820b96cdeeb94703c122df
Shruti Gupta <113260340+shrutigupta263@users.noreply.github.com>
2025-01-31 19:59:56 +0530

    array revision

--- array.js
+++ array.js
@@ -30,7 +30,7 @@ const myn1 =myArr.slice(1,3)
 console.log(myn1);
 
 console.log("B", myArr);
-const myn2 = myArr.slice(1,3)
+const myn2 = myArr.splice(1,3)
 console.log("C",myArr);
 console.log(myn2);
 
